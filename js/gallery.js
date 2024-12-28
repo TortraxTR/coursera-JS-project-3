3653d381772836ac62466ff3cbbab04e8f0887c8
Efe Yılmaz <iceburn322@gmail.com>
2024-12-28 14:12:55 +0300

    Added keyboard accessibility features.

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,22 @@
 /*Name this external file gallery.js*/
 
+function tabAccess() {
+    const images = document.querySelectorAll(".preview");
+    for(let i = 0; i < images.length; i++) {
+        images[i].setAttribute("tabindex", 0);
+        images[i].setAttribute("onfocus", "upDate(this)");
+        images[i].setAttribute("onblur", "unDo()");
+    }
+}
+
 function upDate(previewPic) {
     let imgboard = document.getElementById("image");
     imgboard.style.backgroundImage = "url(" + previewPic.src + ")";
     imgboard.innerHTML = previewPic.alt;
-
+    
 }
 
 function unDo() {
-    
     let imgboard = document.getElementById("image");
     imgboard.style.backgroundImage = "url()";
     imgboard.innerHTML = "Hover over an image below to display here.";
