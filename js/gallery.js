3c6cf3a239de4651e7ecadda6fb3d56f3fadece6
Efe Yılmaz <iceburn322@gmail.com>
2024-12-28 14:21:41 +0300

    Shifting tabAccess position in file.

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,5 @@
 /*Name this external file gallery.js*/
 
-function tabAccess() {
-    const images = document.querySelectorAll(".preview");
-    for(let i = 0; i < images.length; i++) {
-        images[i].setAttribute("tabindex", 0);
-        images[i].setAttribute("onfocus", "upDate(this)");
-        images[i].setAttribute("onblur", "unDo()");
-    }
-}
-
 function upDate(previewPic) {
     let imgboard = document.getElementById("image");
     imgboard.style.backgroundImage = "url(" + previewPic.src + ")";
@@ -21,4 +12,13 @@ function unDo() {
     imgboard.style.backgroundImage = "url()";
     imgboard.innerHTML = "Hover over an image below to display here.";
 
+}
+
+function tabAccess() {
+    const images = document.querySelectorAll(".preview");
+    for(let i = 0; i < images.length; i++) {
+        images[i].setAttribute("tabindex", 0);
+        images[i].setAttribute("onfocus", "upDate(this)");
+        images[i].setAttribute("onblur", "unDo()");
+    }
 }
